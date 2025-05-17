7431e4cb0b8441d5e7cd12435f01491c455cceb8
TariqNew <marandumashaka78@gmail.com>
2025-05-17 05:24:14 +0300

    refactor(entry_point): Adding body parser middlewares to the entry

--- server.js
+++ server.js
@@ -1,13 +1,22 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const contactRoute = require("./routes/contactRoute");
+const errorHandler = require("./middlewares/errorHandler");
 
 const PORT = process.env.PORT || 8080;
 
+// Create app function which acts as the entry point
 const app = express();
 
-//Provide Route for the Contacts
+// Body parser middlewares
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+
+// Provide Route for the Contacts
 app.use("/contact", contactRoute);
+
+// Error handler middleware (must be after routes)
+app.use(errorHandler);
 
 // Start the Express server and listen on the defined PORT
 app.listen(PORT, () => {
