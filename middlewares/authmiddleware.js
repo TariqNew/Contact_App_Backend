cc0cacdd644444a96275eb26643b33d64471c2a2
TariqNew <marandumashaka78@gmail.com>
2025-05-19 12:21:44 +0300

    implement authentication middleware to protect routes

---
+++ middlewares/authmiddleware.js
@@ -0,0 +1,23 @@
+const jwt = require("jsonwebtoken");
+
+const protect = (req, res, next) => {
+  const authHeader = req.headers.authorization;
+
+  if (authHeader && authHeader.startsWith("Bearer ")) {
+    const token = authHeader.split(" ")[1];
+
+    try {
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      req.user = decoded;
+      next();
+    } catch (err) {
+      res.status(401);
+      throw new Error("Not authorized, token failed");
+    }
+  } else {
+    res.status(401);
+    throw new Error("Not authorized, no token");
+  }
+};
+
+module.exports = { protect };
